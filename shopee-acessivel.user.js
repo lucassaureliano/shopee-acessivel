fd259db591570aa9b9d438c865a89567c8aae432
Lucas Aureliano <68451545+lucassaureliano@users.noreply.github.com>
2024-08-20 20:23:48 -0300

    2024.2
    Altera o funcionamento do script para remover todos os aria-hiden, aria-label, alt e adicionar cabeçalho ao título dos produtos.

--- shopee-acessivel.user.js
+++ shopee-acessivel.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Shopee Acessível
 // @namespace    http://tampermonkey.net/
-// @version      2024.1
-// @description  Torna os resultados da busca visíveis para o leitor de telas.
+// @version      2024.2
+// @description  Torna os resultados da busca visíveis para o leitor de telas e adiciona atributos de acessibilidade.
 // @author       Lucas Aureliano
 // @match        https://*.shopee.com.br/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shopee.com.br
@@ -12,20 +12,25 @@
 (function() {
     'use strict';
 
-    function changeAriaHidden() {
-        var elements = document.querySelectorAll('.shopee-search-item-result__items li div div');
-        elements.forEach(function(element) {
-            element.setAttribute('aria-hidden', 'false');
+    function updateAccessibility() {
+        document.querySelectorAll('ul.row.shopee-search-item-result__items > li').forEach(e => {
+            e.querySelectorAll('[aria-hidden]').forEach(a => a.removeAttribute('aria-hidden'));
+            e.querySelectorAll('[aria-label]').forEach(a => a.removeAttribute('aria-label'));
+            e.querySelectorAll('img').forEach(img => img.setAttribute('alt', ''));
+            e.querySelectorAll('div.whitespace-normal').forEach(div => {
+                div.setAttribute('role', 'heading');
+                div.setAttribute('aria-level', '2');
+            });
         });
     }
 
     window.addEventListener('load', function() {
-        changeAriaHidden();
+        updateAccessibility();
     });
 
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
-            changeAriaHidden();
+            updateAccessibility();
         });
     });
 
