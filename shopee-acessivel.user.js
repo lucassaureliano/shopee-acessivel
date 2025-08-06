4c6a11d9b92b47667a4ba9b382707a686e645b3d
Lucas Aureliano <68451545+lucassaureliano@users.noreply.github.com>
2025-08-06 13:35:07 -0300

    2025.8.6b

--- shopee-acessivel.user.js
+++ shopee-acessivel.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shopee Acessível
 // @namespace    http://tampermonkey.net/
-// @version      2025.8.6a
+// @version      2025.8.6b
 // @description  Torna os resultados da busca visíveis para o leitor de telas e adiciona atributos de acessibilidade.
 // @author       Lucas Aureliano
 // @match        https://*.shopee.com.br/*
@@ -23,10 +23,19 @@
                 div.setAttribute('tabindex', '0');
             });
         });
+        let similarHeading = document.querySelector('div.miIYkb');
+        similarHeading.setAttribute('role', 'heading');
+        similarHeading.setAttribute('aria-level', '2');
         let similar_items = document.querySelector('div.rBfdm_.row');
         similar_items.setAttribute('role', 'list');
         similar_items.querySelectorAll('div.wujux8').forEach(item => {
             item.setAttribute('role', 'listitem');
+            item.querySelectorAll('[aria-label]').forEach(a => a.removeAttribute('aria-label'));
+            item.querySelectorAll('img').forEach(img => img.setAttribute('alt', ''));
+            item.querySelectorAll('div.whitespace-normal, div.line-clamp-2').forEach(div => {
+                div.setAttribute('role', 'heading');
+                div.setAttribute('aria-level', '3');
+            });
         });
     }
 
